36fb5f7feb8dfaa0a9d8fa47a2b5632cdc32af7e
sandeepl337 <sandeepk.l337@gmail.com>
2020-01-06 17:13:07 +0530

    initial commit

---
+++ deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+# Update theme before build
+git submodule update --recursive --remote
+
+# Build the project.
+hugo -t slate
+
+cd public
+rsync -a . ../../infosec-startups.github.io/ #Path to target repo
+cd ../
+rm -drf public #Remove build dir from current root
+
+#Switch to target repo
+cd ../infosec-startups.github.io
+
+# Add changes to git.
+git add -A
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Come Back up to the Project Root
+cd ..
